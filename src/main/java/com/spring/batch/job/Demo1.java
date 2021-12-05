97a0f8c66c758353d374d16faabbdc5e43280b5a
QuantumBits1 <muhdafham8@gmail.com>
2021-12-05 10:20:08 +0800

    Initial commit

---
+++ src/main/java/com/spring/batch/job/Demo1.java
@@ -0,0 +1,84 @@
+package com.spring.batch.job;
+
+import com.spring.batch.dto.EmployeeDTO;
+import com.spring.batch.mapper.EmployeeFileRowMapper;
+import com.spring.batch.model.Employee;
+import com.spring.batch.processor.EmployeeProcessor;
+import com.spring.batch.writer.EmployeeDBWriter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Demo1 {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+    private EmployeeProcessor employeeProcessor;
+    private EmployeeDBWriter employeeDBWriter;
+
+    @Autowired
+    public Demo1(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory,
+                 EmployeeProcessor employeeProcessor, EmployeeDBWriter employeeDBWriter){
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.employeeProcessor = employeeProcessor;
+        this.employeeDBWriter = employeeDBWriter;
+    }
+
+    @Qualifier(value = "demo1")
+    @Bean
+    public Job demo1Job() throws Exception {
+        return this.jobBuilderFactory.get("demo1")
+                .start(step1Demo1())
+                .build();
+    }
+
+    @Bean
+    public Step step1Demo1() throws Exception {
+        return this.stepBuilderFactory.get("step1")
+                .<EmployeeDTO, Employee>chunk(5)
+                .reader(employeeReader())
+                .processor(employeeProcessor)  //optional
+                .writer(employeeDBWriter)
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    Resource inputFileResource(@Value("#{jobParameters[fileName]}") final String fileName) throws Exception {
+        return new ClassPathResource(fileName);
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<EmployeeDTO> employeeReader() throws Exception {
+        FlatFileItemReader<EmployeeDTO> reader = new FlatFileItemReader<>();
+        reader.setResource(inputFileResource(null));
+        reader.setLineMapper(new DefaultLineMapper<EmployeeDTO>() {{
+            setLineTokenizer(new DelimitedLineTokenizer() {{
+                setNames("employeeId", "firstName", "lastName", "email", "age");
+                setDelimiter(",");
+            }});
+            setFieldSetMapper(new EmployeeFileRowMapper());
+        }});
+        return reader;
+    }
+
+}
